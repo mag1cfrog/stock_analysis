fa622acf2096398f12c53d79ddf5af827fe579bb
mag1cfrog <harrywong2017@gmail.com>
2024-01-12 11:31:36 -0600

    tests 30-minute stock data

--- src/strategy_construction/default_gold_cross_death_cross.py
+++ src/strategy_construction/default_gold_cross_death_cross.py
@@ -209,14 +209,14 @@ def main():
     db_path = r'E:\git_repos\stock_analysis\data\raw_stock_price\stock_bar_data.db'
     conn = duckdb.connect(db_path)
     query = """
-    SELECT * FROM NVDA_hour_1
+    SELECT * FROM NVDA_minute_30
     """
     stock_df = conn.execute(query).pl().sort('timestamp')
     conn.close()
         
     results_df = sensitivity_analysis(stock_df, short_range, long_range)
     print(results_df)
-    # analyze_and_visualize(results_df)
+    analyze_and_visualize(results_df)
     print(f"Time taken: {time.time() - start_time} seconds")
     run_dash_app(results_df.to_pandas().set_index('Short_Window'))
 
